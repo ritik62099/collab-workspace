02ac6a2e750d1ac497845ff4d39eedbd03b7566c
Ritik kumar <140908124+ritik62099@users.noreply.github.com>
2026-07-11 08:57:25 +0530

    Initial commit: Real-Time Collaborative Workspace setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",  // Ye line bahut important hai
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
